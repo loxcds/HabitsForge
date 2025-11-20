fc7ebd2b3b7e172faed90df31a4b1f40dedc980c
avdeev-dima-tutu <avdeev@tutu.tech>
2025-11-20 13:19:31 +0300

    Сделал пользовательский интерфейс

--- app/src/main/java/com/kulbekk/habitsforge/MainActivity.kt
+++ app/src/main/java/com/kulbekk/habitsforge/MainActivity.kt
@@ -4,44 +4,113 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import com.kulbekk.habitsforge.ui.theme.HabitsForgeTheme
+
+@Preview(showBackground = true, device = Devices.PIXEL)
+@Composable
+fun GreetingPreview() {
+    Main()
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            HabitsForgeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Main()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Main() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Box(Modifier.padding(innerPadding)) {
+            RegistryScreen()
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    HabitsForgeTheme {
-        Greeting("Android")
+fun RegistryScreen() {
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()) {
+        Text("HABITSFORGE")
+        Text("Добро пожаловать в HabitsForge!\n Давайте создадим ваш аккаунт")
+
+        var userName by remember { mutableStateOf("") }
+        TextField(
+            value = userName,
+            onValueChange = { newName ->
+                userName = newName
+            },
+            placeholder = {
+                Text("Введите ваше имя")
+            }
+        )
+
+        var mail by remember { mutableStateOf("") }
+        TextField(
+            value = mail,
+            onValueChange = { newMail ->
+                mail = newMail
+            },
+            placeholder = {
+                Text("Почта")
+            }
+        )
+
+        var password by remember { mutableStateOf("") }
+        TextField(
+            value = password,
+            onValueChange = { newPassword ->
+                password = newPassword
+            },
+            placeholder = {
+                Text("Пароль")
+            },
+            visualTransformation = AsteriskPasswordVisualTransformation())
+
+        Text("Уже есть аккаунт", Modifier.clickable {
+            println("Переход на экран входа")
+        })
+
+    }
+}
+
+/**
+ * Для символов * в пароле
+ */
+class AsteriskPasswordVisualTransformation(
+    private val mask: Char = '*'
+) : VisualTransformation {
+    override fun filter(text: AnnotatedString): TransformedText {
+        val masked = AnnotatedString(mask.toString().repeat(text.length))
+        val offset = object : OffsetMapping {
+            override fun originalToTransformed(offset: Int) = offset
+            override fun transformedToOriginal(offset: Int) = offset
+        }
+        return TransformedText(masked, offset)
     }
 }
